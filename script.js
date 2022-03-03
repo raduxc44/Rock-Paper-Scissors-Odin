c3df54f5230a93ade4a592f84176e7901fb8b903
raduxc44 <raduxc44@gmail.com>
2022-03-03 17:49:31 +0200

    Reworked playRound and game functions

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // The initial variable declarations including the user's pre-picked input with the case sensitive test/
 let computerChoice;
 let userChoice;
+let userWinnerCount = 0;
+let computerWinnerCount = 0;
+let tiesCount = 0;
+let rounds;
 // The function picks a random number that will change the computerChoice variable's value
 function computerPlay () {
     computerChoice = Math.floor(Math.random() * 3);
@@ -28,33 +32,32 @@ function userChoiceCheck () {
     userChoice = userChoice.toLowerCase();
 }
 
+function playerWins () {
+    if((computerChoice === 'rock' && userChoice === 'scissors') || (computerChoice === 'paper' && userChoice === 'rock') || 
+    (computerChoice === 'scissors' && userChoice === 'paper')) {return true};
+}
+function tie () {
+    if(computerChoice === userChoice) {return true};
+}
+function computerWins () { if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'paper' && computerChoice === 'rock') || 
+(userChoice === 'scissors' && computerChoice === 'paper')) {return true};
+}
 // The function will pick a round winner
 
 function playRound () {
     computerPlay();
     userChoice = window.prompt('What is your pick?', '');
     userChoiceCheck()
-    if((computerChoice === 'rock' && userChoice === 'scissors') || (computerChoice === 'paper' && userChoice === 'rock')  || 
-        (computerChoice === 'scissors' && userChoice === 'paper')) {return `You lost! Computer chose ${computerChoice}.`}
-    else if (computerChoice === userChoice) 
-    {return `It's a tie! Computer chose ${computerChoice}.`} 
-    else if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'paper' && computerChoice === 'rock') || 
-            (userChoice === 'scissors' && computerChoice === 'paper')) 
-    {return `You won! Computer chose ${computerChoice}.`}
-    else {return 'It seems like you did not enter a valid weapon.'}
-}
+    if(playerWins()) {userWinnerCount++; console.log(`You won! The computer chose ${computerChoice}`)}
+    else if(tie()) {tiesCount++; console.log(`It's a tie! You both chose ${computerChoice}`)}
+    else if(computerWins()) {computerWinnerCount++; console.log(`You lost. The computer chose ${computerChoice}`)}
+    else {console.log('It seems like you did not enter a valid weapon.')}
+    }
+
 // The function will pick a winner out of 5 rounds also counting ties
 function game () {
-    let userWinnerCount = 0;
-    let computerWinnerCount = 0;
-    let ties = 0;
-    let rounds;
-    for (let i = 0; i < 5; i++) {rounds = playRound();
-        console.log(rounds);
-    if(rounds === `You won! Computer chose ${computerChoice}.`) {userWinnerCount += 1}
-    else if (rounds === `You lost! Computer chose ${computerChoice}.`) {computerWinnerCount += 1}
-    else {ties += 1}
+    for (let i = 0; i < 5; i++) {playRound();
     }
-    console.log(`${userWinnerCount} - You / ${computerWinnerCount} - The computer / ${ties} - Tie(s)`);
+    console.log(`${userWinnerCount} - You / ${computerWinnerCount} - The computer / ${tiesCount} - Tie(s)`);
 }
 game()
