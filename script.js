2749a40e508e6412b04875680d401f0db390ea8c
raduxc44 <raduxc44@gmail.com>
2022-03-03 05:35:07 +0200

    Added an input checker

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // The initial variable declarations including the user's pre-picked input with the case sensitive test/
 let computerChoice;
-let userChoice = window.prompt('What is your pick?', 'Weapon');
+let userChoice = window.prompt('What is your pick?', '');
 // The function picks a random number that will change the computerChoice variable's value
 function computerPlay () {
     computerChoice = Math.floor(Math.random() * 3);
@@ -16,20 +16,26 @@ function computerPlay () {
         break;
     }
 }
-// The function will make the user input case-insensitive
+// The function will check the input and block the rest of the content until the input is valid
 
-function userChoiceLow () {userChoice = userChoice.toLowerCase()}
-userChoiceLow();
+
 function userChoiceCheck () {
-    if(userChoice !== 'rock' || userChoice !== 'paper' || userChoice !== 'scissors') {alert('You did not pick a valid value! Please pick between Rock, Paper or Scissors!')}
+    while (userChoice.toLowerCase() !== 'rock' && userChoice.toLowerCase() !== 'paper' && userChoice.toLowerCase() !== 'scissors') {
+        alert('You did not pick a valid weapon! Please pick between Rock, Paper and Scissors!')
+        userChoice = window.prompt('What is your pick?', '');
+        if(userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {break;}
 }
+    userChoice = userChoice.toLowerCase();
+}
+userChoiceCheck()
 // The function will pick a round winner
 
 function playRound () {
     computerPlay(); 
     if((computerChoice === 'rock' && userChoice === 'scissors') || (computerChoice === 'paper' && userChoice === 'rock') || (computerChoice === 'scissors' && userChoice === 'paper')) {return `You lost! Computer chose ${computerChoice}.`}
-    else if (computerChoice === userChoice) {return `It's a tie! Computer chose ${computerChoice}.`} else if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'paper' && computerChoice === 'rock') || (userChoice === 'scissors' && computerChoice === 'paper')) {return `You won! Computer chose ${computerChoice}.`}
-    else {alert('You did not pick a valid value! Please pick between Rock, Paper or Scissors!')}
+    else if (computerChoice === userChoice) {return `It's a tie! Computer chose ${computerChoice}.`} 
+    else if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'paper' && computerChoice === 'rock') || (userChoice === 'scissors' && computerChoice === 'paper')) {return `You won! Computer chose ${computerChoice}.`}
+    else {return 'It seems like you did not enter a valid weapon.'}
 }
 // The function will pick a winner out of 5 rounds also counting ties
 function game () {
