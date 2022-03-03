9f9ed305296a7ff0f71343fe64f6106f45e157f6
raduxc44 <raduxc44@gmail.com>
2022-03-03 03:38:14 +0200

    winner of 5 not working

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // The initial variable declarations including the user's pre-picked input/
 let computerChoice;
-let userChoice = 'scissors';
-let result;
+let userChoice = 'paper';
+
 // The function picks a random number that will change the computerChoice variable's value
 function computerPlay () {
     computerChoice = Math.floor(Math.random() * 3);
@@ -17,15 +17,25 @@ function computerPlay () {
         break;
     }
 }
-computerPlay();
-// The function will change the result's value according to the game's winner
+
+// The function will pick a round winner
 function userPlay (userChoice) {return userChoice.toLowerCase();}
-function determineWinner () {
-    if((computerChoice === 'rock' && userChoice === 'scissors') || (computerChoice === 'paper' && userChoice === 'rock') || (computerChoice === 'scissors' && userChoice === 'paper')) {result = 'You lost!'}
-    else if (computerChoice === userChoice) {result = 'It\'s a tie!'} else {result = 'You won!'}
-    return result;
+function playRound () {
+    computerPlay(); 
+    if((computerChoice === 'rock' && userChoice === 'scissors') || (computerChoice === 'paper' && userChoice === 'rock') || (computerChoice === 'scissors' && userChoice === 'paper')) {return `You lost! Computer chose ${computerChoice}.`}
+    else if (computerChoice === userChoice) {return `It's a tie! Computer chose ${computerChoice}.`} else {return `You won! Computer chose ${computerChoice}.`}
 }
-determineWinner();
-console.log(`The computer chose ${computerChoice}.`);
-console.log(`You chose ${userChoice}.`);
-console.log(result);
+// The function will pick a winner of 5 rounds *not working*
+function game () {
+    let userWinnerCount = 0;
+    let computerWinnerCount = 0;
+    let ties = 0;
+    for (let i = 0; i < 5; i++) {console.log(playRound());
+    if(playRound() === `You won! Computer chose ${computerChoice}.`) {userWinnerCount += 1}
+    else if (playRound() === `You lost! Computer chose ${computerChoice}.`) {computerWinnerCount += 1}
+    else {ties += 1}
+    }
+    console.log(`${userWinnerCount} - times the user won / ${computerWinnerCount} - times the computer won / ${ties} - Ties`);
+}
+game()
+
