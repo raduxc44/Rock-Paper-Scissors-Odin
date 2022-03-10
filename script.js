da40707e4a7bfd0fe2f97b61076c3a7a0419881a
raduxc44 <raduxc44@gmail.com>
2022-03-10 22:35:16 +0200

    ...

--- script.js
+++ script.js
@@ -6,57 +6,58 @@ let computerWinnerCount = 0;
 let tiesCount = 0;
 let rounds;
 // The function picks a random number that will change the computerChoice variable's value
-function computerPlay () {
+function computerPlay() {
     computerChoice = Math.floor(Math.random() * 3);
     switch (computerChoice) {
         case 0:
             computerChoice = 'rock';
-        break;
+            break;
         case 1:
             computerChoice = 'paper';
-        break;
+            break;
         case 2:
             computerChoice = 'scissors';
-        break;
+            break;
     }
 }
 // The function will check the input and block the rest of the content until the input is valid
 
 
-function userChoiceCheck () {
-    while (userChoice.toLowerCase() !== 'rock' && userChoice.toLowerCase() !== 'paper' && userChoice.toLowerCase() !== 'scissors') {
+function userChoiceCheck() {
+    userChoice = userChoice.toLowerCase();
+    while (userChoice !== 'rock' && userChoice !== 'paper' && userChoice !== 'scissors') {
         alert('You did not pick a valid weapon! Please pick between Rock, Paper and Scissors!')
         userChoice = window.prompt('What is your pick?', '');
-        if(userChoice === 'rock' || userChoice === 'paper' || userChoice === 'scissors') {break;}
-}
-    userChoice = userChoice.toLowerCase();
+    }
 }
 
-function playerWins () {
-    if((computerChoice === 'rock' && userChoice === 'scissors') || (computerChoice === 'paper' && userChoice === 'rock') || 
-    (computerChoice === 'scissors' && userChoice === 'paper')) {return true};
+function playerWins() {
+    if ((computerChoice === 'rock' && userChoice === 'scissors') || (computerChoice === 'paper' && userChoice === 'rock') ||
+        (computerChoice === 'scissors' && userChoice === 'paper')) { return true };
 }
-function tie () {
-    if(computerChoice === userChoice) {return true};
+function tie() {
+    if (computerChoice === userChoice) { return true };
 }
-function computerWins () { if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'paper' && computerChoice === 'rock') || 
-(userChoice === 'scissors' && computerChoice === 'paper')) {return true};
+function computerWins() {
+    if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'paper' && computerChoice === 'rock') ||
+        (userChoice === 'scissors' && computerChoice === 'paper')) { return true };
 }
 // The function will pick a round winner
 
-function playRound () {
+function playRound() {
     computerPlay();
     userChoice = window.prompt('What is your pick?', '');
     userChoiceCheck()
-    if(playerWins()) {userWinnerCount++; console.log(`You won! The computer chose ${computerChoice}`)}
-    else if(tie()) {tiesCount++; console.log(`It's a tie! You both chose ${computerChoice}`)}
-    else if(computerWins()) {computerWinnerCount++; console.log(`You lost. The computer chose ${computerChoice}`)}
-    else {console.log('It seems like you did not enter a valid weapon.')}
-    }
+    if (playerWins()) { userWinnerCount++; console.log(`You won! The computer chose ${computerChoice}`) }
+    else if (tie()) { tiesCount++; console.log(`It's a tie! You both chose ${computerChoice}`) }
+    else if (computerWins()) { computerWinnerCount++; console.log(`You lost. The computer chose ${computerChoice}`) }
+    else { console.log('It seems like you did not enter a valid weapon.') }
+}
 
 // The function will pick a winner out of 5 rounds also counting ties and show each's score
-function game () {
-    for (let i = 0; i < 5; i++) {playRound();
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound();
     }
     console.log(`${userWinnerCount} - You / ${computerWinnerCount} - The computer / ${tiesCount} - Tie(s)`);
 }
