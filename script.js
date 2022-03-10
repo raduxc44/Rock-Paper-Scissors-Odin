a81ee8a263a7490a5d87d14818a0563103987c4c
raduxc44 <raduxc44@gmail.com>
2022-03-10 23:37:40 +0200

    Added the user choice buttons

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // The initial variable declarations including the user's pre-picked input with the case sensitive test/
 let computerChoice;
-let userChoice;
 let userWinnerCount = 0;
 let computerWinnerCount = 0;
 let tiesCount = 0;
@@ -22,43 +21,58 @@ function computerPlay() {
 }
 // The function will check the input and block the rest of the content until the input is valid
 
-
-function userChoiceCheck() {
-    userChoice = userChoice.toLowerCase();
-    while (userChoice !== 'rock' && userChoice !== 'paper' && userChoice !== 'scissors') {
-        alert('You did not pick a valid weapon! Please pick between Rock, Paper and Scissors!')
-        userChoice = window.prompt('What is your pick?', '');
-    }
-}
-
-function playerWins() {
-    if ((computerChoice === 'rock' && userChoice === 'scissors') || (computerChoice === 'paper' && userChoice === 'rock') ||
-        (computerChoice === 'scissors' && userChoice === 'paper')) { return true };
-}
-function tie() {
-    if (computerChoice === userChoice) { return true };
-}
-function computerWins() {
-    if ((userChoice === 'rock' && computerChoice === 'scissors') || (userChoice === 'paper' && computerChoice === 'rock') ||
-        (userChoice === 'scissors' && computerChoice === 'paper')) { return true };
-}
 // The function will pick a round winner
 
-function playRound() {
+function playRound(userChoice) {
     computerPlay();
-    userChoice = window.prompt('What is your pick?', '');
-    userChoiceCheck()
-    if (playerWins()) { userWinnerCount++; console.log(`You won! The computer chose ${computerChoice}`) }
-    else if (tie()) { tiesCount++; console.log(`It's a tie! You both chose ${computerChoice}`) }
-    else if (computerWins()) { computerWinnerCount++; console.log(`You lost. The computer chose ${computerChoice}`) }
+
+    function playerWins() {
+        if ((computerChoice === 'paper' && userChoice === 'scissors') || (computerChoice === 'scissors' && userChoice === 'rock') ||
+            (computerChoice === 'rock' && userChoice === 'paper')) { return true };
+    }
+    playerWins()
+    function tie() {
+        if (computerChoice === userChoice) { return true };
+    }
+    tie()
+    function computerWins() {
+        if ((userChoice === 'rock' && computerChoice === 'paper') || (userChoice === 'paper' && computerChoice === 'scissors') ||
+            (userChoice === 'scissors' && computerChoice === 'rock')) { return true };
+    }
+    computerWins();
+
+
+
+
+
+
+    if (playerWins()) { console.log(`You won! The computer chose ${computerChoice}`) }
+    else if (tie()) { console.log(`It's a tie! You both chose ${computerChoice}`) }
+    else if (computerWins()) { console.log(`You lost. The computer chose ${computerChoice}`) }
     else { console.log('It seems like you did not enter a valid weapon.') }
 }
 
 // The function will pick a winner out of 5 rounds also counting ties and show each's score
-function game() {
-    for (let i = 0; i < 5; i++) {
-        playRound();
-    }
-    console.log(`${userWinnerCount} - You / ${computerWinnerCount} - The computer / ${tiesCount} - Tie(s)`);
-}
-game()
+const body = document.querySelector('body');
+const div = document.createElement('div');
+div.style = 'background-color : yellow; height: 100px; display : flex; align-items: center;';
+body.appendChild(div);
+const buttons = document.createElement('div');
+div.appendChild(buttons);
+buttons.style = 'display:flex; width: 100%; height : 25%; justify-content : space-around;';
+const buttonOne = document.createElement('button');
+const buttonTwo = document.createElement('button');
+const buttonThree = document.createElement('button');
+buttons.appendChild(buttonOne);
+buttons.appendChild(buttonTwo);
+buttons.appendChild(buttonThree);
+buttonOne.classList.add('btn');
+buttonOne.innerText = 'Rock';
+buttonTwo.innerText = 'Paper';
+buttonThree.innerText = 'Scissors';
+buttonTwo.classList.add('btn');
+buttonThree.classList.add('btn');
+const btn = document.querySelector('btn');
+buttonOne.addEventListener('click', () => { playRound('rock') });
+buttonTwo.addEventListener('click', () => { playRound('paper') });
+buttonThree.addEventListener('click', () => { playRound('scissors') })
